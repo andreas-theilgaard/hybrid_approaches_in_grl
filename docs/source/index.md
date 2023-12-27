e4385ecf0653ab54d4998e41fb98e9018bbbb455
Andreas Theilgaard <actheilgaard@gmail.com>
2023-12-27 10:03:25 +0100

    initial commit

---
+++ docs/source/index.md
@@ -0,0 +1,4 @@
+## Documentation
+
+Documentation for Hybrid Approaches in GRL
+
---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+from Hybrid Approaches in GRL.models.model import MyNeuralNet
+from Hybrid Approaches in GRL.predict_model import predict
